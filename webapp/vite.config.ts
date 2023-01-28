01a92ae6c39d33f0b30293bbad4806e14bf858e5
Lee Martin <hello@leejohnmartin.co.uk>
2023-01-28 21:25:56 +0000

    Init: proxy config

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -2,8 +2,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const proxy = {
+  "/api": {
+    target: "http://localhost:3012",
+    changeOrigin: true,
+    secure: false,
+    ws: true,
+  },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  preview: {
+    port: 3000,
+  },
+  server: {
+    port: 3000,
+    proxy,
+  },
   plugins: [react()],
   test: {
     setupFiles: "test-setup.ts",
