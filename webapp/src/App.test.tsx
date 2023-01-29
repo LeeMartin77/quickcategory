aff92a29a3ac273c22202e302b88e49664dba06c
Lee Martin <hello@leejohnmartin.co.uk>
2023-01-29 22:35:35 +0000

    Init: Rigging in react router

--- webapp/src/App.test.tsx
+++ webapp/src/App.test.tsx
@@ -1,53 +1,16 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { MockedProvider } from "@apollo/client/testing";
-import userEvent from "@testing-library/user-event";
-import App, { TEST_QUERY } from "./App";
+import { MemoryRouter } from "react-router-dom";
+import App from "./App";
 
-test("loads and displays greeting", async () => {
-  // ARRANGE
-  render(
-    <MockedProvider>
-      <App />
-    </MockedProvider>
-  );
-
-  // ACT
-  const button = screen.getByText("count is 0");
-  await userEvent.click(button);
-  await screen.findByRole("heading");
-
-  // ASSERT
-  expect(screen.getByRole("heading")).toHaveTextContent("Vite + React");
-  expect(screen.getByText("count is 1")).toBeInTheDocument();
-});
-
-test("loads and displays gql response", async () => {
-  // ARRANGE
-  const mocks = [
-    {
-      request: {
-        query: TEST_QUERY,
-
-        variables: {},
-      },
-
-      result: {
-        data: {
-          test: { hello: "test-world" },
-        },
-      },
-    },
-  ];
+test("landing on a bad page", () => {
+  const badRoute = "/some/bad/route";
 
   render(
-    <MockedProvider mocks={mocks}>
+    <MemoryRouter initialEntries={[badRoute]}>
       <App />
-    </MockedProvider>
+    </MemoryRouter>
   );
 
-  // ACT
-
-  // ASSERT
-  expect(await screen.findByText("test-world")).toBeInTheDocument();
+  expect(screen.getByText(/Not Found/i)).toBeInTheDocument();
 });
--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
 
 const client = new ApolloClient({
   uri: "/api/graphql",
@@ -13,7 +14,9 @@ const client = new ApolloClient({
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ChakraProvider>
     <ApolloProvider client={client}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ApolloProvider>
   </ChakraProvider>
 );
